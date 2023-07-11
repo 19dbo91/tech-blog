bb03961af0dc149527b97b5f530fcbefa9fa80c0
Dustin Bonilla <bonilla.dustin@gmail.com>
2023-07-10 23:16:54 -0700

    added connection to sequelize

--- server.js
+++ server.js
@@ -2,4 +2,25 @@
 //#region Dependencies
 const sql = require('mysql2');
 const express = require('express');
+const db = require('./config/connection.js')
+
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//#endregion
+
+//here
+
+
+
+
+
+
+
+
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
