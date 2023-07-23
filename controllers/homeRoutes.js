4f9529e8ec836abab0554f9074c288543be5e118
Dustin Bonilla <bonilla.dustin@gmail.com>
2023-07-23 01:00:21 -0700

    updated dashboard route and handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,8 +24,9 @@ router.get('/', async (req, res) =>{
             return post.get( {plain:true} );
         });
         //console.log(blogposts);
-
+        var title = "The Tech Blog";
         res.status(200).render('homepage',{
+            title,
             loggedIn,
             blogposts
         });
@@ -36,7 +37,33 @@ router.get('/', async (req, res) =>{
 
 });
 
-// Dashboard shows
+// Dashboard shows MY titles+date as buttons and a new 
+router.get('/dashboard', async (req, res) =>{
+    try{
+        //check if logged in 
+        var loggedIn = true;
+
+        const blogpostData = await BlogPost.findAll({
+            where: { id:1 },
+            include:{ model: User },
+            order: [['createdAt', 'ASC']]//need a where its my posts
+
+        });
+        const blogposts = blogpostData.map( (post) => {
+            return post.get( {plain:true} );
+        });
+        
+        var title = "Your Dashboard";
+        res.status(200).render('dashboard',{
+            title,
+            loggedIn,
+            blogposts
+        });
+    }catch(err){
+        console.error(err);
+        res.status(500).json(`Error 500: Internal server error...\n ${err}`);
+    }
+});
 
 // Login shows
 
